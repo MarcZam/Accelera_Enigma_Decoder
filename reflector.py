52c220fc46e69794b1a37e3ccddef9f687a8a3e3
MarcZam <74371778+MarcZam@users.noreply.github.com>
2021-02-21 20:12:27 +0100

    Delete reflector.py

--- reflector.py
+++
@@ -1,8 +0,0 @@
-class Reflector():
-
-    def __init__(self, conf = ["ABCDEFGHIJKLNÑMOPQRSTUVWXYZ", "ZYXWVUTSRQPOMÑNLKJIHGFEDCBA"]):
-        self.configuracion = conf
-
-    def refleja(self, letra_entrada):
-        posE = self.configuracion[0].index(letra_entrada)
-        return self.configuracion[1][posE]
